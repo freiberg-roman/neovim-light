0d96a7840e217b7a7f76b2f26ef25de6a0eb3dbd
Roman Freiberg <roman.sascha@googlemail.com>
2022-12-30 12:15:11 +0100

    proper base setup nvim

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 # ### REQUIREMENTS ###
 #
-# font: IBM Plex Mono
+# font: Blex Mono Nerd Font (patched)
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -17,4 +17,6 @@ ln -s $SCRIPT_DIR/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 mkdir -p $HOME/.config/nvim
 rm $HOME/.config/nvim/init.vim  # in case it already exists
 rm $HOME/.config/nvim/init.lua  # in case it already exists
+rm -r $HOME/.config/nvim/lua # remove lua config
 ln -s $SCRIPT_DIR/nvim/init.lua $HOME/.config/nvim/init.lua
+ln -s $SCRIPT_DIR/nvim/lua $HOME/.config/nvim/lua
